97ef0adf98f31b84c95e2f55e19f160c8aa1ce61
SameGevDudios <magomed.chapaev1@mail.ru>
2025-10-24 23:49:55 +0300

    Add basic structure

--- LR2_Debug_and_Trace/LR2_Debug_and_Trace/Program.cs
+++ LR2_Debug_and_Trace/LR2_Debug_and_Trace/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LR2_Debug_and_Trace;
+
+new Startup()
+    //TODO: .InitTask1()
+    .Run()
+    .PrintResult()
+    //TODO: .InitTask2()
+    .Run()
+    .PrintResult();
---
+++ LR2_Debug_and_Trace/LR2_Debug_and_Trace/Startup.cs
@@ -0,0 +1,23 @@
+﻿using LR2_Debug_and_Trace.Abstractions;
+using System.Diagnostics;
+
+namespace LR2_Debug_and_Trace
+{
+    public class Startup
+    {
+        private ITask _task = null!;
+
+        public Startup Run()
+        {
+            Debug.Assert(_task != null);
+            _task.Run();
+            return this;
+        }
+
+        public Startup PrintResult(string message = "Task done")
+        {
+            Console.WriteLine($"{message}: {_task.Result()}");
+            return this;
+        }
+    }
+}
