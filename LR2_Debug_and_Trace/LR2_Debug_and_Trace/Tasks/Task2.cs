80742e53a9eb8a63e6eedf88862fd6d7fd193b1a
SameGevDudios <magomed.chapaev1@mail.ru>
2025-10-25 09:24:54 +0300

    Add Task 2

--- LR2_Debug_and_Trace/LR2_Debug_and_Trace/Tasks/Task2.cs
+++ LR2_Debug_and_Trace/LR2_Debug_and_Trace/Tasks/Task2.cs
@@ -1,17 +1,47 @@
 ﻿using LR2_Debug_and_Trace.Abstractions;
+using System.Diagnostics;
 
 namespace LR2_Debug_and_Trace.Tasks
 {
-    internal class Task2 : ITask
+    internal class Task2 : PracticeTask
     {
-        public void Run()
+        private int _from = 1,
+            _to;
+
+        public Task2(int to)
         {
-            throw new NotImplementedException();
+            _to = to;
         }
 
-        public double Result()
+        protected override void CalculateResult()
         {
-            throw new NotImplementedException();
+            Result = CalculateSum();
+        }
+
+        private int CalculateSum()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < _to; i++)
+            {
+                int factorial = Factorial(_from, i);
+                try
+                {
+                    sum = checked(sum + factorial);
+                }
+                catch (Exception e) 
+                {
+                    Trace.WriteLine($"Error occured trying to sum values: {e.Message}");
+                }
+            }
+            return sum;
+        }
+
+        private int Factorial(int from, int to)
+        {
+            int result = 1;
+            for (int i = 1; i <= to; i++) result *= i;
+            return result;
         }
     }
 }
