1a2b208a9fb3db7d01ce9b10ebcdd3388ead2888
SameGevDudios <magomed.chapaev1@mail.ru>
2025-10-25 09:50:38 +0300

    Add input extension

---
+++ LR2_Debug_and_Trace/LR2_Debug_and_Trace/Extensions/WaitInputExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace LR2_Debug_and_Trace.StartupExtensions
+{
+    public static class WaitInputExtension
+    {
+        public static Startup WaitForInput(this Startup startup)
+        {
+            Console.ReadLine();
+            return startup;
+        }
+    }
+}
--- LR2_Debug_and_Trace/LR2_Debug_and_Trace/Program.cs
+++ LR2_Debug_and_Trace/LR2_Debug_and_Trace/Program.cs
@@ -2,9 +2,11 @@
 using LR2_Debug_and_Trace.StartupExtensions;
 
 new Startup()
+    .WaitForInput()
     .InitializeTask1(a: .1, b: 1, function: x => Math.Log(1 + x) / x)
     .Run()
     .PrintResult()
+    .WaitForInput()
     .Whitespace()
     .InitializeTask2(10)
     .Run()
