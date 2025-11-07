cb20897676a34702adaa1d58dc2859ba8beaf800
SameGevDudios <magomed.chapaev1@mail.ru>
2025-11-07 22:41:49 +0300

    Update Program.cs

--- LR3_Black_Box_Testing/LR3_Black_Box_Testing/Program.cs
+++ LR3_Black_Box_Testing/LR3_Black_Box_Testing/Program.cs
@@ -2,27 +2,19 @@
 using LR3_Black_Box_Testing.Core;
 using System.Numerics;
 
-
-IPointChecker _checker = new LineChecker(10);
-
-Console.WriteLine("Классы эквивалентности: 3. Точка в окружности, в фигуре.");
-Console.WriteLine("1. Точка вне окружности - |x| или |y| > 10.");
-Console.WriteLine("2. Точка в окружности, вне фигуры - |x| или |y| <= 10.");
-Console.WriteLine("2. Точка в окружности, в фигуре - угол [pi/4, pi/2], [5pi/4, 3pi/2].");
-Console.Write("Введите целочисленные координаты точек x y: ");
-
-Vector2 _point = ParsePoint(Console.ReadLine());
-
-_checker.SetPoint(_point);
-_checker.Check();
-
-Console.WriteLine($"Результат: {_checker.GetResult()}");
-
-static Vector2 ParsePoint(string input)
+// Objects initialization
+ICollection<Vector2> _data = new List<Vector2>()
 {
-    var p = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    return new Vector2(
-        int.Parse(p[0]),
-        int.Parse(p[1])
-    );
-}
+    new Vector2(10, 1),
+    new Vector2(10, 2),
+    new Vector2(10, 3),
+};
+
+CheckerController _controller = new TraceCheckerController(new LineChecker(size: 10));
+
+// Run
+_controller
+    .PrintInfo()
+    .SetupData(_data)
+    .Run()
+    .PrintResult();
