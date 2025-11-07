c4ee63e907b14cc30254f613e0e8f587aca11ff1
SameGevDudios <magomed.chapaev1@mail.ru>
2025-11-07 16:11:31 +0300

    Add checker abstraction

---
+++ LR3_Black_Box_Testing/LR3_Black_Box_Testing/Abstractions/IPointChecker.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace LR3_Black_Box_Testing.Abstractions
+{
+    public interface IPointChecker
+    {
+        void SetPoint(Vector2 point);
+
+        void Check();
+
+        int GetResult();
+    }
+}
