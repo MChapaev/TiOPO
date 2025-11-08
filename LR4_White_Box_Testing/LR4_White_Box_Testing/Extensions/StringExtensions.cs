35d260b1e6c26be509ee64683e09822625179d31
SameGevDudios <magomed.chapaev1@mail.ru>
2025-11-08 19:08:58 +0300

    Add trim method

---
+++ LR4_White_Box_Testing/LR4_White_Box_Testing/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace LR4_White_Box_Testing.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Trim(this string str)
+        {
+            if (str == null) throw new ArgumentNullException(nameof(str));
+            if (str.Length < 3) return string.Empty;
+            char[] chars = new char[str.Length - 2];
+            for (int i = 1; i <= chars.Length; i++) chars[i] = str[i];
+            return new string(chars);
+        }
+    }
+}
