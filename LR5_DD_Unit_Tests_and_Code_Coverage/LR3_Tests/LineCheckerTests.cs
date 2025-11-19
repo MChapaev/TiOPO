8e5bac543f59ef948665848f7b6f6b31d571a675
MChapaev <magomed.chapaev1@mail.ru>
2025-11-19 22:36:22 +0300

    Switch to param testing

--- LR5_DD_Unit_Tests_and_Code_Coverage/LR3_Tests/LineCheckerTests.cs
+++ LR5_DD_Unit_Tests_and_Code_Coverage/LR3_Tests/LineCheckerTests.cs
@@ -8,72 +8,49 @@ namespace LR3_Tests
     {
         private const int _size = 10;
 
-        private float _square1 => (float)Math.Sqrt(_size);
-        private float _square2 => (float)Math.Sqrt(_size / 2f);
+        private static float _square1 => (float)Math.Sqrt(_size);
+        private static float _square2 => (float)Math.Sqrt(_size / 2f);
 
-        private LineChecker checker = null!;
+        public static IEnumerable<TestCaseData> Points =>
+            new List<TestCaseData>
+            {
+                // Outside shape & outside boundary. Result = 1
+                new TestCaseData(new Vector2(_square1 + 0.001f, 0f), 1)
+                    .SetName("OutsideShape_OutsideBoundary_Point1"),
 
-        [SetUp]
-        public void Setup()
+                new TestCaseData(new Vector2(_square1 + 1f, 1f), 1)
+                    .SetName("OutsideShape_OutsideBoundary_Point2"),
+
+                // Outside shape & inside boundary. Result = 2
+                new TestCaseData(new Vector2(_square1 - 0.001f, 0f), 2)
+                    .SetName("OutsideShape_InsideBoundary_Point1"),
+
+                new TestCaseData(new Vector2(_square2, -_square2), 2)
+                    .SetName("OutsideShape_InsideBoundary_Point2"),
+
+                // Inside shape & inside boundary. Result = 3
+                new TestCaseData(new Vector2(_square2 / 2f, _square2 / 2f), 3)
+                    .SetName("InsideShape_InsideBoundary_Point1"),
+
+                new TestCaseData(new Vector2(0.5f, 1f), 3)
+                    .SetName("InsideShape_InsideBoundary_Point2"),
+
+                new TestCaseData(new Vector2(-_square2, -_square2), 3)
+                    .SetName("InsideShape_InsideBoundary_Point3"),
+
+                new TestCaseData(new Vector2(-1f, -1.5f), 3)
+                    .SetName("InsideShape_InsideBoundary_Point4"),
+            };
+
+        [Test]
+        [TestCaseSource(nameof(Points))]
+        public void TestLineChecker(Vector2 point, int expected)
         {
-            checker = new LineChecker(_size);
-        }
-
-        private void AssertResult(Vector2 point, int expected)
-        {
+            var checker = new LineChecker(_size);
             checker.SetPoint(point);
             checker.Check();
 
-            Assert.That(checker.GetResult(), Is.EqualTo(expected),
-                () => $"Expected result {expected} for point {point}");
-        }
-
-        [Test]
-        public void OutsideShape_OutsideBoundary_Point1()
-        {
-            AssertResult(new Vector2(_square1 + 0.001f, 0f), 1);
-        }
-
-        [Test]
-        public void OutsideShape_OutsideBoundary_Point2()
-        {
-            AssertResult(new Vector2(_square1 + 1f, 1f), 1);
-        }
-
-        [Test]
-        public void OutsideShape_InsideBoundary_Point1()
-        {
-            AssertResult(new Vector2(_square1 - 0.001f, 0f), 2);
-        }
-
-        [Test]
-        public void OutsideShape_InsideBoundary_Point2()
-        {
-            AssertResult(new Vector2(_square2, -_square2), 2);
-        }
-
-        [Test]
-        public void InsideShape_InsideBoundary_Point1()
-        {
-            AssertResult(new Vector2(_square2 / 2f, _square2 / 2f), 3);
-        }
-
-        [Test]
-        public void InsideShape_InsideBoundary_Point2()
-        {
-            AssertResult(new Vector2(0.5f, 1f), 3);
-        }
-
-        [Test]
-        public void InsideShape_InsideBoundary_Point3()
-        {
-            AssertResult(new Vector2(-_square2, -_square2), 3);
-        }
-
-        [Test]
-        public void InsideShape_InsideBoundary_Point4()
-        {
-            AssertResult(new Vector2(-1f, -1.5f), 3);
+            Assert.That(checker.GetResult(), Is.EqualTo(expected));
         }
     }
 }
